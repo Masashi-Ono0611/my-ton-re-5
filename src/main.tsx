98f8928d1b841815d285e06884153175fe707869
masashiono0611 <masashi.ono.0611@gmail.com>
2025-03-18 22:30:49 +0800

    GitHub Pages用の設定を更新

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,8 @@ import App from './App';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-// GitHub Pagesデプロイに合わせたマニフェストURL
-const manifestUrl = '/my-ton-re-5/tonconnect-manifest.json';
+// GitHub Pages用のマニフェストURL
+const manifestUrl = 'https://masashi-ono0611.github.io/my-ton-re-5/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
